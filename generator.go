00d8962cdb64d25634cb425101f7b5ef1f603057
Arshiya Kiani <zytell3301@gmail.com>
2021-11-05 17:20:49 +0330

    Add comment for GenerateV5 method

--- generator.go
+++ generator.go
@@ -19,6 +19,10 @@ func NewGenerator(space string) (*Generator, error) {
 	return &Generator{space: uuidSpace}, nil
 }
 
+// This method treats as a hashing function. But be careful that
+// same space and same name will result in same uuid. So the
+// guarantee of the uniqueness of the generated UUIDs is application's
+// responsibility
 func (g Generator) GenerateV5(name string) string {
 	return uuid.NewSHA1(g.space, []byte(name)).String()
 }
