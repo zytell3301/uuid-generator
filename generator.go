070bbd631c648f6779f4504f38bf7c5fc0407447
Arshiya Kiani <zytell3301@gmail.com>
2022-03-22 01:21:05 +0430

    Change v4 uuid generation to worker based

--- generator.go
+++ generator.go
@@ -6,12 +6,26 @@ import (
 
 type Generator struct {
 	space uuid.UUID
+
+	v4Buffer         chan uuid.UUID
+	v4StopSignal     chan struct{}
+	v4GenerateSignal chan struct{}
+	bufferSize       int
+	workerCount      int
 }
 
-func NewGenerator(space string) (*Generator, error) {
+func NewGenerator(space string, bufferSize int, workerCount int) (*Generator, error) {
+	generator := Generator{
+		bufferSize:       bufferSize,
+		workerCount:      workerCount,
+		v4Buffer:         make(chan uuid.UUID, bufferSize),
+		v4GenerateSignal: make(chan struct{}),
+		v4StopSignal:     make(chan struct{}),
+	}
+	generator.startV4Workers()
 	switch space == "" {
 	case true:
-		return &Generator{}, nil
+		return &generator, nil
 	}
 
 	uuidSpace, err := uuid.Parse(space)
@@ -19,8 +33,35 @@ func NewGenerator(space string) (*Generator, error) {
 	case true:
 		return nil, err
 	}
+	generator.space = uuidSpace
+	return &generator, nil
+}
 
-	return &Generator{space: uuidSpace}, nil
+func (g Generator) startV4Workers() {
+	for i := 0; i < g.bufferSize; i++ {
+		g.v4Generator()
+	}
+}
+
+func (g Generator) refillBuffer() {
+	for i := 0; i < (g.bufferSize - len(g.v4Buffer)); i++ {
+		g.v4GenerateSignal <- struct{}{}
+	}
+}
+
+func (g Generator) v4Generator() {
+	for {
+		select {
+		case <-g.v4GenerateSignal:
+			uuid, err := uuid.NewRandom()
+			switch err == nil {
+			case true:
+				g.v4Buffer <- uuid
+			}
+		case <-g.v4StopSignal:
+			return
+		}
+	}
 }
 
 // This method treats as a hashing function. But be careful that
@@ -35,12 +76,8 @@ func (g Generator) GenerateV5(name string) *uuid.UUID {
 // This method returns a fully random UUID (UUID v4).
 // Returned UUID will be nil if an error occurred while
 // generating the uuid
-func (Generator) GenerateV4() (*uuid.UUID, error) {
-	uuid, err := uuid.NewRandom()
-	switch err != nil {
-	case true:
-		return nil, err
-	}
-
-	return &uuid, nil
+func (g Generator) GenerateV4() *uuid.UUID {
+	uuid := <-g.v4Buffer
+	g.refillBuffer()
+	return &uuid
 }
