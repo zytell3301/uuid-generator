fa35c6a8fc81c2f565c5e453b480cef674a444ee
Arshiya Kiani <zytell3301@gmail.com>
2022-03-22 14:53:43 +0430

    Add InvalidCheckerIntervalSuppliedError error

--- errors.go
+++ errors.go
@@ -3,6 +3,7 @@ package uuid_generator
 import "errors"
 
 var (
-	CheckerAlreadyStoppedError = errors.New("reader checker already stopped")
-	CheckerAlreadyStartedError = errors.New("reader checker already started")
+	CheckerAlreadyStoppedError          = errors.New("reader checker already stopped")
+	CheckerAlreadyStartedError          = errors.New("reader checker already started")
+	InvalidCheckerIntervalSuppliedError = errors.New("invalid number for checker interval supplied")
 )
